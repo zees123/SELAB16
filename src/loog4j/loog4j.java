6e64b7f4ff404a83721081efe9bc61059d3ea904
muhammad <muhammad@172.16.105.225>
2016-05-31 05:15:42 -0700

    zeeshan

---
+++ src/loog4j/loog4j.java
@@ -0,0 +1,21 @@
+package loog4j;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
+
+public class loog4j {
+	private static Logger logger = Logger.getLogger(loog4j.class);
+	public static void main(String [] args){
+		//DOMConfigurator.configure("log4j.xml");
+		PropertyConfigurator.configure("log4j.properties");
+		//BasicConfigurator.configure();
+		try{
+			int i = 0;
+			i = i / 0;
+		}catch(Exception e){
+			logger.error(e.getMessage());
+		}
+	}
+}
+
+ 
