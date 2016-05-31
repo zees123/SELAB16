f5e05b46fd9fd820bfe23066a71bffb8833463ab
muhammad <muhammad@172.16.105.225>
2016-05-31 05:30:36 -0700

    my second commit

--- src/loog4j/loog4j.java
+++ src/loog4j/loog4j.java
@@ -4,6 +4,7 @@ import org.apache.log4j.PropertyConfigurator;
 
 
 public class loog4j {
+	//int x=10;
 	private static Logger logger = Logger.getLogger(loog4j.class);
 	public static void main(String [] args){
 		//DOMConfigurator.configure("log4j.xml");
